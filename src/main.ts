83288d19dec52a63c314ff982c415609e46940de
SergiFont <sfontsmx2010@gmail.com>
2023-05-04 19:06:46 +0200

    first commit, first steps with sockets

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { connectToServer } from './socket-client'
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Websocket - Client</h1>
+
+    <input id="jwtToken" placeholder="Json Web Token" />
+    <button id="btn-connect">Connect</button>
+
+    <br/>
+
+    <span id='server-status'>offline</span>
+
+    <ul id="clients-ul"></ul>
+
+    <form id="message-form">
+      <input placeholder="message" id="message-input" />
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
+
+  </div>
+`
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+// connectToServer()
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwtToken')
+const btnConnect = document.querySelector<HTMLInputElement>('#btn-connect')
+
+btnConnect?.addEventListener('click', () => {
+
+  if ( jwtToken?.value.trim().length! <= 0) return alert('Enter a valid JWT')
+
+  connectToServer( jwtToken!.value.trim() )
+
+})
